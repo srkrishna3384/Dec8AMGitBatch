efdb532843a2b7edde20a365599cb2132e5b566f
srkrishna3384 <srkrishna3384@gmail.com>
2021-01-05 08:48:31 +0530

    Added December8AM project

---
+++ src/test/java/com/rameshsoft/sample/TestOne.java
@@ -0,0 +1,21 @@
+package com.rameshsoft.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestOne {
+	
+	@Test
+	public void test() {
+		System.out.println("TestOne Class.....");
+		//System.setProperty(key, value)
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rameshsoft.com");
+		System.out.println("Navigated to Rameshsoft web page");
+	}
+
+}
---
+++ src/test/java/com/rameshsoft/sample/TestTwo.java
@@ -0,0 +1,20 @@
+package com.rameshsoft.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestTwo {
+	@Test
+	public void demo() {
+		System.out.println("TesTwo Class.....");
+		//System.setProperty(key, value)
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com");
+		System.out.println("Navigated to News18 web page");
+	}
+
+}
